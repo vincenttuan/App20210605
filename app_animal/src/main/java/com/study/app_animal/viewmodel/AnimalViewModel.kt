e6ec077cfcce694dbac9ccb3e2e937485dcba5a4
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-05 15:48:59 +0800

    App

---
+++ app_animal/src/main/java/com/study/app_animal/viewmodel/AnimalViewModel.kt
@@ -0,0 +1,17 @@
+package com.study.app_animal.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.study.app_animal.model.Animal
+
+class AnimalViewModel: ViewModel() {
+    var animal: Animal? = null
+
+    val currentImageURL: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val currentInfo: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+}
