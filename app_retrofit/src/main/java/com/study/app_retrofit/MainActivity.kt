41b9fd6d9a8238b27ce10ca763a5125ef4d3ee1b
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-12 10:36:39 +0800

    App

--- app_retrofit/src/main/java/com/study/app_retrofit/MainActivity.kt
+++ app_retrofit/src/main/java/com/study/app_retrofit/MainActivity.kt
@@ -2,10 +2,25 @@ package com.study.app_retrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.study.app_retrofit.manager.RetrofitManager
+import com.study.app_retrofit.model.Post
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Call
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            val api = RetrofitManager.instance.api
+            val posts: Call<List<Post>> = api.getPosts()
+            val postList = posts.execute().body()
+            Log.d("MainActivity", postList.toString())
+
+        }
+
     }
 }
