0ac839bc8ac7ccc1f5626f73b3a994b2e448369b
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-12 10:26:09 +0800

    App

--- app_retrofit/src/main/java/com/study/app_retrofit/api/JsonPlaceHolderApi.kt
+++ app_retrofit/src/main/java/com/study/app_retrofit/api/JsonPlaceHolderApi.kt
@@ -4,6 +4,7 @@ import com.study.app_retrofit.model.Post
 import retrofit2.Call
 import retrofit2.http.GET
 
+// API 的目的是為了給 client 調用
 interface JsonPlaceHolderApi {
     @GET("posts")
     fun getPosts(): Call<List<Post>>
---
+++ app_retrofit/src/main/java/com/study/app_retrofit/manager/RetrofitManager.kt
@@ -0,0 +1,26 @@
+package com.study.app_retrofit.manager
+
+import com.study.app_retrofit.api.JsonPlaceHolderApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+// 會以 Sington 來設計
+class RetrofitManager {
+    private val placeHolderApi: JsonPlaceHolderApi
+    val api: JsonPlaceHolderApi
+        get() = placeHolderApi
+
+    companion object {
+        val instance = RetrofitManager()
+    }
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        placeHolderApi = retrofit.create(JsonPlaceHolderApi::class.java)
+    }
+
+}
