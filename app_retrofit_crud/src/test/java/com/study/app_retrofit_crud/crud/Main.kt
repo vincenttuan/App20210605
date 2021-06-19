37236892c03e90a046090666d619b2540e59f0a5
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-19 10:10:54 +0800

    App

--- app_retrofit_crud/src/test/java/com/study/app_retrofit_crud/crud/Main.kt
+++ app_retrofit_crud/src/test/java/com/study/app_retrofit_crud/crud/Main.kt
@@ -1,5 +1,8 @@
 package com.study.app_retrofit_crud.crud
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -13,9 +16,23 @@ interface JsonPlaceHolderService {
 }
 
 fun main() {
+    val okHttpClient = OkHttpClient().newBuilder()
+        .addInterceptor(object : Interceptor {
+            override fun intercept(chain: Interceptor.Chain): Response {
+                // User-Agent 加上 "我是 chrome etc..."
+                val oldRequest = chain.request()
+                val newRequest = oldRequest.newBuilder()
+                    .header("User-Agent", "chrome")
+                    .build()
+                return chain.proceed(newRequest)
+            }
+        })
+        .build()
+
     val retrofit = Retrofit.Builder()
         .baseUrl("https://jsonplaceholder.typicode.com/")
         .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
         .build()
     val api = retrofit.create(JsonPlaceHolderService::class.java)
 
