634940f2828ecbe203216b2c41259bd3db125424
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-19 10:00:27 +0800

    App

--- app_retrofit_crud/src/test/java/com/study/app_retrofit_crud/crud/Main.kt
+++ app_retrofit_crud/src/test/java/com/study/app_retrofit_crud/crud/Main.kt
@@ -1,5 +1,26 @@
 package com.study.app_retrofit_crud.crud
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+// API (Service)
+interface JsonPlaceHolderService {
+    @GET("posts/{id}")
+    fun getPost(@Path("id") id: Int?): Call<Post?>?
+}
+
 fun main() {
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://jsonplaceholder.typicode.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    val api = retrofit.create(JsonPlaceHolderService::class.java)
+
     println("CRUD...")
+    // Read
+    val post = api.getPost(1)
+    println(post!!.execute().body())
 }
---
+++ app_retrofit_crud/src/test/java/com/study/app_retrofit_crud/crud/Post.kt
@@ -0,0 +1,4 @@
+package com.study.app_retrofit_crud.crud
+
+data class Post(var userId:Int?, var id: Int?, var title: String?, var body: String?) {
+}
