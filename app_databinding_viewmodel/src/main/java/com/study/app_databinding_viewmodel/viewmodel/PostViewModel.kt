c9c625b9d5b673a467a0ee4d4ace138ed8b6c8c7
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-26 11:42:53 +0800

    App

--- app_databinding_viewmodel/src/main/java/com/study/app_databinding_viewmodel/viewmodel/PostViewModel.kt
+++ app_databinding_viewmodel/src/main/java/com/study/app_databinding_viewmodel/viewmodel/PostViewModel.kt
@@ -1,5 +1,6 @@
 package com.study.app_databinding_viewmodel.viewmodel
 
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.github.javafaker.Faker
@@ -10,6 +11,14 @@ class PostViewModel : ViewModel() {
     var post = MutableLiveData<Post>()
 
     fun click() {
+        val jf = Faker()
+        post.value = Post(
+            Random.nextInt(100),
+            jf.book().title(),
+            jf.book().author()
+        )
+    }
+    fun click2(view: View) {
         val jf = Faker()
         post.value = Post(
             Random.nextInt(100),
