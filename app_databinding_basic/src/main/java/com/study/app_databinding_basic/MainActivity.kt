4c1bdb037d089ea0df36b3bae06bdf668ce2b37f
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-26 10:47:15 +0800

    App

--- app_databinding_basic/src/main/java/com/study/app_databinding_basic/MainActivity.kt
+++ app_databinding_basic/src/main/java/com/study/app_databinding_basic/MainActivity.kt
@@ -2,10 +2,13 @@ package com.study.app_databinding_basic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 // 根據 layout 配置是否支援 databinding 來動態產生
 import com.study.app_databinding_basic.databinding.ActivityMainBinding
 import com.study.app_databinding_basic.model.Post
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     lateinit var mBinding: ActivityMainBinding
@@ -19,6 +22,12 @@ class MainActivity : AppCompatActivity() {
         // 建立資料
         val post = Post(12, "Kotlin", "Java")
         // 繫結資料 Binding data
+        mBinding.post = post
+    }
+
+    fun click(view: View) {
+        val post = Post(Random.nextInt(100), "Python", "Vincent")
+        Toast.makeText(this, post.toString(), Toast.LENGTH_SHORT).show()
         mBinding.post = post
     }
 }
