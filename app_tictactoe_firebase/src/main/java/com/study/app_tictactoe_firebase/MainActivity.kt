e55408e8b24efaf1d24110d4dc3cffcb9168bf87
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-03 15:06:07 +0800

    App

--- app_tictactoe_firebase/src/main/java/com/study/app_tictactoe_firebase/MainActivity.kt
+++ app_tictactoe_firebase/src/main/java/com/study/app_tictactoe_firebase/MainActivity.kt
@@ -1,26 +1,60 @@
 package com.study.app_tictactoe_firebase
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val database = Firebase.database
     val myTTTRef = database.getReference("ttt/game")
-
+    lateinit var context: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        context = this
         bt_mark.setOnClickListener {
-            val mark = bt_mark.text.toString()
+            val mark = bt_mark.tag.toString()
             if(mark.equals("O")) {
                 bt_mark.text = "X"
+                bt_mark.tag = "X"
             } else {
                 bt_mark.text = "O"
+                bt_mark.tag = "O"
             }
         }
+
+        myTTTRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val game = snapshot.children
+                Log.d("MainActivity", game.toString())
+                game.forEach {
+                    Log.d("MainActivity", it.key + ":" + it.value)
+                    val id = resources.getIdentifier(it.key, "id", context.packageName)
+                    findViewById<Button>(id).text = it.value.toString()
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    fun tttOnClick(view: View) {
+        val tag = view.getTag().toString()
+        val path = "b" + tag
+        val mark = bt_mark.tag.toString()
+        myTTTRef.child(path).setValue(mark)
     }
 }
