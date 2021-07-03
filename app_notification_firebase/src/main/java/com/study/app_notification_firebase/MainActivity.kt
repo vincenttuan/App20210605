3db3dbf85a06b22e55e2a3fc1ff4823db4f6da93
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-03 12:28:00 +0800

    App

--- app_notification_firebase/src/main/java/com/study/app_notification_firebase/MainActivity.kt
+++ app_notification_firebase/src/main/java/com/study/app_notification_firebase/MainActivity.kt
@@ -2,11 +2,17 @@ package com.study.app_notification_firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,6 +25,24 @@ class MainActivity : AppCompatActivity() {
             val data = et_message.text.toString()
             myMessageRef.setValue(data)
         }
+
+        // Read from the database
+        myMessageRef.addValueEventListener(object: ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = snapshot.getValue<String>()
+                tv_message.text = value
+                Log.d(TAG, "Value is: " + value)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+
+        })
+
 
 
     }
