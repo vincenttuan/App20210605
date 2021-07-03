466b3fec2ba39bf93cfbca077f60d05d6f32721a
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-03 14:17:03 +0800

    App

--- app_tictactoe_firebase/src/main/java/com/study/app_tictactoe_firebase/MainActivity.kt
+++ app_tictactoe_firebase/src/main/java/com/study/app_tictactoe_firebase/MainActivity.kt
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val database = Firebase.database
+    val myTTTRef = database.getReference("ttt/game")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val database = Firebase.database
-        val myTTTRef = database.getReference("ttt/game")
-
-        myTTTRef.child("b1").setValue("O")
-        myTTTRef.child("b2").setValue("X")
-        myTTTRef.child("b3").setValue("O")
+        bt_mark.setOnClickListener {
+            val mark = bt_mark.text.toString()
+            if(mark.equals("O")) {
+                bt_mark.text = "X"
+            } else {
+                bt_mark.text = "O"
+            }
+        }
     }
 }
