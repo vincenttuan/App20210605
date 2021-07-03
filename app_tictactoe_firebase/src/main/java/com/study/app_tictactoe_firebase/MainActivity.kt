aa33b6ec21208a02f145d2316f89e621e1c1ef34
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-03 14:08:42 +0800

    App

--- app_tictactoe_firebase/src/main/java/com/study/app_tictactoe_firebase/MainActivity.kt
+++ app_tictactoe_firebase/src/main/java/com/study/app_tictactoe_firebase/MainActivity.kt
@@ -2,10 +2,19 @@ package com.study.app_tictactoe_firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = Firebase.database
+        val myTTTRef = database.getReference("ttt/game")
+
+        myTTTRef.child("b1").setValue("O")
+        myTTTRef.child("b2").setValue("X")
+        myTTTRef.child("b3").setValue("O")
     }
 }
