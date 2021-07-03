1e22cca8651f2767759e9c802fbf3e172c613f36
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-03 12:14:58 +0800

    App

--- app_notification_firebase/src/main/java/com/study/app_notification_firebase/MainActivity.kt
+++ app_notification_firebase/src/main/java/com/study/app_notification_firebase/MainActivity.kt
@@ -2,10 +2,19 @@ package com.study.app_notification_firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+
     }
 }
