5eb996cf9ffa29ea0785627681f613ff8415bdbc
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-10 13:50:57 +0800

    App

--- app_ticket_firebase/src/main/java/com/study/app_ticket_firebase/MainActivity.kt
+++ app_ticket_firebase/src/main/java/com/study/app_ticket_firebase/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.study.app_ticket_firebase
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    lateinit var context: Context
+    lateinit var userName: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        context = this
+        userName = intent.getStringExtra("userName").toString()
+        title = String.format(title.toString(), userName)
+                             // 雲端購票 - %s
+
     }
 }
