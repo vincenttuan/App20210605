67ee3fd748dc175411a91f81a62765019d042b0f
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-10 14:13:16 +0800

    App

--- app_ticket_firebase/src/main/java/com/study/app_ticket_firebase/MainActivity.kt
+++ app_ticket_firebase/src/main/java/com/study/app_ticket_firebase/MainActivity.kt
@@ -3,11 +3,14 @@ package com.study.app_ticket_firebase
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.study.app_ticket_firebase.models.TicketsStock
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val database = Firebase.database
@@ -15,6 +18,8 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var context: Context
     lateinit var userName: String
+    lateinit var ticketsStock: TicketsStock
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,22 +30,33 @@ class MainActivity : AppCompatActivity() {
         // Set activity title
         title = String.format(title.toString(), userName)
                              // 雲端購票 - %s
+        // new TicketsStock
+        ticketsStock = TicketsStock(0.0, 0, 0)
         // Read from the database
         myRef.addValueEventListener(object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 val children = snapshot.children
                 children.forEach {
                     when (it.key.toString()) {
-                        "discount" -> { }
-                        "price" -> { }
-                        "totalAmount" -> { }
+                        "discount" -> ticketsStock.discount = it.value.toString().toDouble()
+                        "price" -> ticketsStock.price = it.value.toString().toInt()
+                        "totalAmount" -> ticketsStock.totalAmount = it.value.toString().toInt()
                     }
                 }
+                // refresh ui
+                refreshUI()
             }
             override fun onCancelled(error: DatabaseError) {
-
+                Toast.makeText(context, error.toString(), Toast.LENGTH_SHORT).show()
             }
         })
 
     }
+
+    fun refreshUI() {
+        tv_ticket_discount.text = ticketsStock.discount.toString()
+        tv_ticket_price.text = ticketsStock.price.toString()
+        tv_total_amount.text = ticketsStock.totalAmount.toString()
+    }
+
 }
---
+++ app_ticket_firebase/src/main/java/com/study/app_ticket_firebase/models/TicketsStock.kt
@@ -0,0 +1,7 @@
+package com.study.app_ticket_firebase.models
+
+data class TicketsStock(
+    var discount: Double,
+    var price: Int,
+    var totalAmount: Int
+)
