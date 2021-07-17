d0043907089756d177a09bb15bc1e4b7d74a5fe8
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-17 10:28:49 +0800

    App

--- app_ticket_firebase/src/main/java/com/study/app_ticket_firebase/OrderListActivity.kt
+++ app_ticket_firebase/src/main/java/com/study/app_ticket_firebase/OrderListActivity.kt
@@ -1,24 +1,53 @@
 package com.study.app_ticket_firebase
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_order_list.*
 
 class OrderListActivity : AppCompatActivity() {
+    val database = Firebase.database
+    val myRef = database.getReference("ticketsStock")
+
+    lateinit var context: Context
     lateinit var userName: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_list)
+        context = this
         // 設定 app title
         title = resources.getString(R.string.activity_order_list_title_text)
         // 取得使用者名稱 (上一頁傳來的 userName 參數資料)
         userName = intent.getStringExtra("userName").toString()
         // 判斷是否有 userName 的資料
-        if (userName == null || userName.equals("") || userName.equals("null")) {
-            title = String.format(title.toString(), resources.getString(R.string.all_order_text))
+        title = if (userName == null || userName.equals("") || userName.equals("null")) {
+            String.format(title.toString(), resources.getString(R.string.all_order_text))
         } else {
-            title = String.format(title.toString(), userName)
+            String.format(title.toString(), userName)
         }
 
+        // Read from the databse
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val children = snapshot.children
+                var keys = ""
+                children.forEach {
+                    keys += it.key.toString() + ","
+                }
+                tv_info.text = keys
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
 
 
     }
