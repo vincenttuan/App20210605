a87ab887c2d3ce9650c550d34244d8ac49f44ea3
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-24 11:52:44 +0800

    App

--- app_ticket_firebase/src/main/java/com/study/app_ticket_firebase/ConsoleActivity.kt
+++ app_ticket_firebase/src/main/java/com/study/app_ticket_firebase/ConsoleActivity.kt
@@ -11,6 +11,9 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_console.*
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import java.nio.charset.Charset
 
 class ConsoleActivity : AppCompatActivity() {
     private val database = Firebase.database
@@ -66,6 +69,8 @@ class ConsoleActivity : AppCompatActivity() {
                                 "總單程票：${ String.format("%,d", sumOneWay) } 張\n" +
                                 "總來回票：${ String.format("%,d", sumRoundTrip) } 張\n" +
                                 "總銷售額：${ String.format("%,d", sumTotal) } 元"
+                // 顯示統計圖
+                loadChart()
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -73,6 +78,32 @@ class ConsoleActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    private fun loadChart() {
+        var webSettings = web_view.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.builtInZoomControls = true
+        var asset_path = "file:///android_asset/"
+        var html = getHTML("chart.html")
+
+        web_view.loadDataWithBaseURL(asset_path, html!!, "text/html", "UTF-8", null)
+        web_view.requestFocusFromTouch()
+    }
+
+    private fun getHTML(filename: String): String? {
+        var html: String? = null
+        try {
+            val inputStream: InputStream = assets.open(filename)
+            val out = ByteArrayOutputStream()
+            val buffer = ByteArray(inputStream.available())
+            inputStream.read(buffer) // 資料讀出
+            out.write(buffer) // 資料寫入
+            html = String(out.toByteArray(), Charset.forName("UTF-8"))
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return html
     }
 
     fun update(view: View) {
