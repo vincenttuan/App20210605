9fa2420b4a0dc5e908635a9c1349b6772064e238
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-31 10:27:01 +0800

    App

---
+++ app_login_firebase/src/main/java/com/study/app_login_firebase/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.study.app_login_firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+        Log.d("MainActivity", "currentUser: $currentUser")
+    }
+}
