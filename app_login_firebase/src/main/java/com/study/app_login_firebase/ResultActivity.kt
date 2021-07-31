cb78a81c9552fd85bcec52bd30c350548a3978bd
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-31 14:16:15 +0800

    App

--- app_login_firebase/src/main/java/com/study/app_login_firebase/ResultActivity.kt
+++ app_login_firebase/src/main/java/com/study/app_login_firebase/ResultActivity.kt
@@ -3,11 +3,16 @@ package com.study.app_login_firebase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        val message = intent.getStringExtra("message")
+        if(message != null) {
+            tv_result.text = message
+        }
     }
 
     fun back(view: View) {
--- app_login_firebase/src/main/java/com/study/app_login_firebase/SignUpActivity.kt
+++ app_login_firebase/src/main/java/com/study/app_login_firebase/SignUpActivity.kt
@@ -5,17 +5,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     private lateinit var context: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
         context = this;
+        auth = Firebase.auth
     }
 
     fun signUp(view: View) {
+        val email = et_signup_email.text.toString()
+        val password = et_signup_password.text.toString()
+        var message: String? = ""
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if(task.isSuccessful) {
+                    message = "Sign up success"
+                } else {
+                    message = "Sign up fail: ${task.result}";
+                }
+            }
+
         val intent = Intent(context, ResultActivity::class.java)
+        intent.putExtra("message", message)
         startActivity(intent)
         finish()
     }
