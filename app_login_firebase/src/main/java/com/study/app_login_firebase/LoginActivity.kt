eae56b44be96294a042372a41dd0a0030524e4e7
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-31 11:01:51 +0800

    App

--- app_login_firebase/src/main/java/com/study/app_login_firebase/LoginActivity.kt
+++ app_login_firebase/src/main/java/com/study/app_login_firebase/LoginActivity.kt
@@ -1,11 +1,42 @@
 package com.study.app_login_firebase
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var context: Context
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        context = this
+        auth = Firebase.auth
+
+    }
+
+    fun signIn(view: View) {
+        val email = et_email.text.toString()
+        val password = et_password.text.toString()
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(context, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(context, "Login fail !", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app_login_firebase/src/main/java/com/study/app_login_firebase/MainActivity.kt
+++ app_login_firebase/src/main/java/com/study/app_login_firebase/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.study.app_login_firebase
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,9 +11,11 @@ import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth;
+    private lateinit var context: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        context = this
         auth = Firebase.auth
     }
 
